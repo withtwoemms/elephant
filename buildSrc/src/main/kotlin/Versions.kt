3fb1a03f9f81aa45d3029494bedb6c78b17873ef
Emmanuel I. Obi <withtwoemms@gmail.com>
2018-11-04 16:54:10 -0600

    adds junit version and specifies junit deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     val jvmTarget = "1.8"
     val kotlin = "1.2.71"
-    val kotlinDsl = "1.0-rc-14"
     val okHttp3 = "3.11.0"
+    val junit = "5.3.1"
 }
--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,4 +1,6 @@
 extra {
     "okHttp3" to "com.squareup.okhttp3:${Versions.okHttp3}"
     "kotlinGradlePlugin" to "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    "junitApi" to "org.junit.jupiter:junit-jupiter-api:${Versions.junit}" // for "testImplementation"
+    "junitEngine" to "org.junit.jupiter:junit-jupiter-engine:${Versions.junit}" // for "testRuntimeOnly"
 }
